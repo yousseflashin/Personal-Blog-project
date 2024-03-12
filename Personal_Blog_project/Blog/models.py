b7d4d263dad853f9411b6672befb575990e814da
yousseflashin <youssef.mlashin.yml@gmail.com>
2024-03-12 19:00:05 +0200

    Blog posts V 1.1

--- Personal_Blog_project/Blog/models.py
+++ Personal_Blog_project/Blog/models.py
@@ -6,5 +6,5 @@ class Posts(models.Model):
   sender_name=models.ForeignKey(User,on_delete=models.CASCADE)
   time_created=models.DateField(auto_now_add=True)
   last_updated=models.DateField(auto_now=True)
-  title=models.CharField(max_length=50)
-  content=models.CharField(max_length=500)
+  title=models.CharField(max_length=50,blank=False)
+  content=models.CharField(max_length=500,blank=False)
